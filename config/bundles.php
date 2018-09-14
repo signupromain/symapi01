0e77085fc50db5799b41c7dd147bee4c187c3501
mikhawa <michaeljpitz@gmail.com>
2018-09-14 14:16:41 +0200

    upgrate vendor server-web :arrow_up:

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    // web server bundle install
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
